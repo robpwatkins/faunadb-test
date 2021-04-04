4cc2d03c9319bfd64b7bf657a1dc794d6af56204
robpwatkins <robpwatkins@gmail.com>
2021-04-04 12:57:35 -0500

    easter lunch time

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import './App.css';
 
 function App() {
   const [thought, setThought] = useState({ date: new Date().toISOString().split('T')[0], text: ''});
+  const [memories, setMemories] = useState();
+
+  const getMemories = async () => {
+    const resp = await fetch('/api/memories');
+    const data = await resp.json();
+    setMemories(data);
+  }
+
+  const memCard = (m, i) => <div key={i}>{m.data.text}</div>
+  
+  const renderMemories = memories ? memories.map(memCard) 
+    : <button onClick={getMemories}>Show memories</button>
 
   const saveThought = async () => {
     const resp = await fetch('/api/post-memory', { 
@@ -16,12 +28,13 @@ function App() {
   const handleThoughtChange = e => setThought({ ...thought, [e.target.name]: e.target.value });
 
   return (
-    <div className="App">
-      <h1>Memories</h1>
-      <input type="date" name="date" value={thought.date} onChange={handleThoughtChange} />
-      <input type="text" name="text" placeholder="Your thought" value={thought.text} onChange={handleThoughtChange} />
-      <button onClick={saveThought}>Commit to memory</button>
-    </div>
+    <div id="memories">{renderMemories}</div>
+    // <div className="App">
+    //   <h1>Memories</h1>
+    //   <input type="date" name="date" value={thought.date} onChange={handleThoughtChange} />
+    //   <input type="text" name="text" placeholder="Your thought" value={thought.text} onChange={handleThoughtChange} />
+    //   <button onClick={saveThought}>Commit to memory</button>
+    // </div>
   );
 }
 
